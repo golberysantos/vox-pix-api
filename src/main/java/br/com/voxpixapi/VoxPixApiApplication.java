214324384de9f1a50f9bdc9d512d0606429242fd
Golbery Santos <golbery.santos@yahoo.com.br>
2026-07-27 10:42:39 -0400

    feat: initial project skeleton from Spring Initializr

---
+++ src/main/java/br/com/voxpixapi/VoxPixApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.voxpixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxPixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxPixApiApplication.class, args);
+	}
+
+}
